0d29109eb7a3ca1fb49b89ed6d7443730e072eb9
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-02-25 11:59:31 +0100

    🎨 Update: select_recipes() takes recipe_dir as argument

--- select_recipes.py
+++ select_recipes.py
@@ -1,36 +1,33 @@
 import os
+import glob
 import sys
 import random
-import defaults
 
 # TODO: logging verwenden <25-02-2024>
 
 
-def select_recipes(num_recipes) -> list[str]:
+def select_recipes(num_recipes, recipe_dir) -> list[str]:
     """Randomly selects submitted number of recipes.
 
     :param int num_recipes: Number of recipes
     """
     # Check if the directory exists
-    if not os.path.isdir(defaults.recipe_dir):
-        print(f"Error: Directory '{defaults.recipe_dir}' not found.")
+    if not os.path.isdir(recipe_dir):
+        print(f"Error: Directory '{recipe_dir}' not found.")
         sys.exit(1)
 
     # Check if there are files in the directory
-    num_files = len(os.listdir(defaults.recipe_dir))
     if num_files == 0:
-        print(f"No files found in the directory '{defaults.recipe_dir}'.")
+        print(f"No files found in the directory '{recipe_dir}'.")
         sys.exit(1)
 
-    if 1 > num_recipes > num_files:
         print(f"<num_recipes> must be within 1 and {num_files}.")
         sys.exit(1)
 
-    # Get a list of files in the directory
-    files = [os.path.join(defaults.recipe_dir, f) for f in os.listdir(defaults.recipe_dir)]
+    # # Get a list of files in the directory
+    # files = [os.path.join(recipe_dir, f) for f in os.listdir(recipe_dir)]
 
     # Generate an array of random indices within the range of the number of files
     indices = random.sample(range(num_files), num_recipes)
 
     # Loop through the randomly chosen indices and get the corresponding files
-    return [files[index] for index in indices]
