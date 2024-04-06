b62351e1480171edeeac09dd93e2ce477b784d20
Philipp Rost <philipp.rost@dlr.de>
2024-04-06 09:57:12 +0200

    ♻️ Refactor(test): Use conftest.py instead of class

--- tests/test_GroceryShopper.py
+++ tests/test_GroceryShopper.py
@@ -3,70 +3,54 @@ from grocery_shopper.ingredient import Ingredient
 from grocery_shopper.handle_ing_miss_url import query_for_url
 from grocery_shopper.parse_edited_list import parse_edited_list
 import os
-import pytest
 from collections import Counter
 
 
-class TestGroceryShopper():
+icu_file = os.path.join('tests', 'test_ingredient_category_url.csv')
 
-    """This class is for testing 'grocery-shopper'."""
 
-    icu_file = os.path.join('tests', 'test_ingredient_category_url.csv')
+def filter_names(ingredients: list[Ingredient], names: set[str]) -> list[Ingredient]:
+    return [ing for ing in ingredients if ing.name in names]
 
-    @pytest.fixture
-    def ings_missing_cu(self):
-        return [Ingredient('Orange', 2, meal='Testgericht für build_ingredients_test.py'),
-                Ingredient('Erdbeere', 5, meal='Testgericht für build_ingredients_test.py')]
 
-    @pytest.fixture
-    def ings_with_cu(self):
-        ing_kirsche = Ingredient('Kirsche',
-                                 16,
-                                 optional=False,
-                                 category='Obst',
-                                 url='https://de.wikipedia.org/wiki/Vogelkirsche_(Baum)',
-                                 meal='Testgericht für build_ingredients_test.py')
-        ing_paprika = Ingredient('Paprika',
-                                 1,
-                                 optional=True,
-                                 category='Gemüse',
-                                 url=['https://de.wikipedia.org/wiki/Paprika'],
-                                 meal='Testgericht für build_ingredients_test.py')
-        return [ing_kirsche, ing_paprika]
+def test_query_for_url(monkeypatch, tmp_path, ings_missing_cu):
+    inputs = [(cat0 := 'Category 0'), (url0 := 'URL-2'),
+              (cat1 := 'Category 1'), (url1 := 'URL-1.1 URL-1.2'),
+              (cat2 := 'Category 2'), (url2 := 'URL-2')]
+    monkeypatch.setattr('builtins.input', lambda _: inputs.pop(0))
 
-    def test_query_for_url(self, monkeypatch, tmp_path, ings_missing_cu):
-        inputs = [(cat1 := 'Category 1'), (url1 := 'URL-1'),
-                  (cat2 := 'Category 2'), (url2 := 'URL-2.1 URL-2.2')]
-        monkeypatch.setattr('builtins.input', lambda _: inputs.pop(0))
+    tmp_icu_file = tmp_path / 'ingredient_category_url.csv'
+    # print(icu_file)
 
-        tmp_icu_file = tmp_path / 'ingredient_category_url.csv'
-        # print(icu_file)
+    query_for_url(list(ings_missing_cu), tmp_icu_file)
 
-        query_for_url(ings_missing_cu, tmp_icu_file)
+    ing0 = f'{ings_missing_cu[0].name},{cat0},{url0}'
+    ing1 = f'{ings_missing_cu[1].name},{cat1},{url1.replace(" ", ",")}'
+    ing2 = f'{ings_missing_cu[2].name},{cat2},{url2}'
+    expected_contents = '\n'.join((ing0, ing1, ing2))
+    assert tmp_icu_file.read_text() == expected_contents
 
-        expected_contents = f'{ings_missing_cu[0].name},{cat1},{url1}\n{ings_missing_cu[1].name},{cat2},{url2.replace(" ", ",")}'
-        assert tmp_icu_file.read_text() == expected_contents
 
-    def test_build_ingredients(self, ings_missing_cu, ings_with_cu):
-        build_ingredients = read_icu_file(self.icu_file)
-        recipe_file = os.path.join('tests', 'recipe_build_ingredients.yaml')
-        ings_with_cu_built, ings_missing_cu_built = build_ingredients(recipe_file)
+def test_build_ingredients(ings_missing_cu, ings_with_cu):
+    expected_ings_missing_cu = filter_names(ings_missing_cu, {"Orange", "Erdbeere"})
+    expected_ings_with_cu = filter_names(ings_with_cu, {"Kirsche", "Paprika"})
+    build_ingredients = read_icu_file(icu_file)
+    recipe_file = os.path.join('tests', 'recipe_build_ingredients.yaml')
 
-        assert Counter(ings_missing_cu) == Counter(ings_missing_cu_built)
-        assert Counter(ings_with_cu) == Counter(ings_with_cu_built)
+    bi_ings_with_cu, bi_ings_missing_cu = build_ingredients(recipe_file)
 
-    def test_parse_edited_list(self, ings_missing_cu, ings_with_cu):
-        # from grocery_shopper.make_table import make_table
-        # table: str = make_table([*ings_missing_cu, *ings_with_cu])
-        # print(table)
-        kirsche, paprika = ings_with_cu
-        orange, erdbeere = ings_missing_cu
+    assert Counter(expected_ings_missing_cu) == Counter(bi_ings_missing_cu)
+    assert Counter(expected_ings_with_cu) == Counter(bi_ings_with_cu)
 
-        all_ingredients: list[Ingredient] = [kirsche, paprika, orange, erdbeere]
-        shopping_list_file: str = os.path.join('tests', 'shopping_list_test.txt')
 
-        expected_final_ingredients: list[Ingredient] = [kirsche, orange]
+def test_parse_edited_list(all_ingredients):
+    # from grocery_shopper.make_table import make_table
+    # table: str = make_table(all_ingredients)
+    # print(table)
+    shopping_list_file: str = os.path.join('tests', 'shopping_list_test.txt')
 
-        final_ingredients: list[Ingredient] = parse_edited_list(shopping_list_file, all_ingredients)
+    expected_final_ingredients: list[Ingredient] = filter_names(all_ingredients, {"Orange", "Erdbeere", "Kirsche", "Paprika", "Kürbis", "Brombeere"})
 
-        assert Counter(expected_final_ingredients) == Counter(final_ingredients)
+    final_ingredients: list[Ingredient] = parse_edited_list(shopping_list_file, all_ingredients)
+
+    assert Counter(expected_final_ingredients) == Counter(final_ingredients)
