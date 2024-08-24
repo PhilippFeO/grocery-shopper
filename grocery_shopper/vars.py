1dd91ab881704bb71c71c8b5d36670d8b9cf3c4a
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-08-24 09:39:45 +0200

    ✨ Feat: Global Variables for the directories

--- grocery_shopper/vars.py
+++ grocery_shopper/vars.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 defaults_file: str = 'defaults.ini'
 directories: dict[str, str] = {
     'recipe_dir': 'recipes',
@@ -5,3 +7,7 @@ directories: dict[str, str] = {
     'resource_dir': '.resources',
     'archive_dir': '.archive',
 }
+RECIPE_DIR = Path('./recipes')
+MISC_DIR = Path('./misc')
+RESOURCE_DIR = Path('./.resources')
+ARCHIVE_DIR = Path('./.archive')
