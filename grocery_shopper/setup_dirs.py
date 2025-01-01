e2a743e110d6fb95c88e8dd2dbf7c05c657bda49
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-01-01 22:28:40 +0100

    feat: ✨ Use Recipe class in setup_dirs.py

--- grocery_shopper/setup_dirs.py
+++ grocery_shopper/setup_dirs.py
@@ -1,25 +1,27 @@
-import os
 import configparser
-import sys
 import logging
-from grocery_shopper.vars import directories
-from grocery_shopper.vars import defaults_file
+import sys
+from pathlib import Path
+
+from grocery_shopper.vars import defaults_file, directories
 
 
-def setup_dirs_helper(path: str) -> None:
-    for _, v in directories.items():
-        tmp_dir = os.path.join(path, v)
+def setup_dirs_helper(path: Path) -> None:
+    for v in directories.values():
+        tmp_dir = Path(path, v)
         try:
-            os.makedirs(tmp_dir)
+            tmp_dir.mkdir(parents=True)
         except OSError:
-            logging.warning(
-                f"(Re)move (possible old) directoy '{tmp_dir}' or change location completely.")
+            msg = f"(Re)move (possible old) directoy '{tmp_dir}' or change location completely."
+            logging.warning(msg)
             sys.exit(2)
 
 
-def setup_dirs(config: configparser.ConfigParser,
-               defaults_file_path: str = f'{os.path.dirname(__file__)}/{defaults_file}') -> str:
-    """Checks if necessary directories (s. `vars.py`) exists and if not, creates them.
+def setup_dirs(
+    config: configparser.ConfigParser,
+    defaults_file_path: Path = Path(Path(__file__).parent, defaults_file),
+) -> Path:
+    """Check if necessary directories (s. `vars.py`) exists and if not, creates them.
 
     :config: Config with default values set by user.
     :defaults_file_path: Path to the `defaults.ini` file
@@ -27,17 +29,17 @@ def setup_dirs(config: configparser.ConfigParser,
     """
     # Check if directory was already set, ie program was ran at least once
     try:
-        general_dir = config['general']['dir']
+        general_dir = Path(config['general']['dir'])
     # If not (probably on first run), then set it to CWD
     # Except block entered on first run => directories don't exists
     except (configparser.NoSectionError, configparser.NoOptionError, KeyError):
-        general_dir = os.getcwd()
+        general_dir = Path.cwd()
         # Call to setup_dirs_helper() has to come before setting config
         # The function might exit, then no values should be written to config
         setup_dirs_helper(general_dir)
-        config['general']['dir'] = os.path.expanduser(general_dir)
+        config['general']['dir'] = str(Path.expanduser(general_dir))
         # Write default values
-        with open(defaults_file_path, 'w') as f:
+        with defaults_file_path.open('w') as f:
             config.write(f)
 
     return general_dir
--- tests/test_setup_dirs.py
+++ tests/test_setup_dirs.py
@@ -1,13 +1,15 @@
-from grocery_shopper.setup_dirs import setup_dirs, setup_dirs_helper
-import pytest
 import configparser
+from pathlib import Path
+
+import pytest
+
+from grocery_shopper.setup_dirs import setup_dirs, setup_dirs_helper
 from grocery_shopper.vars import directories
-import os
 
 
-def isdir_helper(path):
-    for _, v in directories.items():
-        assert os.path.isdir(path / v)
+def isdir_helper(path: Path):
+    for v in directories.values():
+        assert Path.is_dir(path / v)
 
 
 @pytest.fixture
@@ -23,48 +25,57 @@ def test_setup_dirs_helper(tmp_path):
     isdir_helper(tmp_path)
 
 
-@pytest.mark.parametrize("dir_name", directories.values())
+@pytest.mark.parametrize('dir_name', directories.values())
 def test_setup_dirs_helper_one_exists(tmp_path, dir_name):
     """Test setup_dirs() in case on directory already exists.
 
     :tmp_path: Path where directories were created
     :dir_name: One of the values of vars.directories
     """
-    os.mkdir(tmp_path / dir_name)
+    (tmp_path / dir_name).mkdir()
     with pytest.raises(SystemExit) as e:
         setup_dirs_helper(tmp_path)
-    assert e.type == SystemExit
+    assert e.type is SystemExit
     assert e.value.code == 2
 
 
 def test_setup_dirs_first_run(monkeypatch, tmp_path, config):
-    """First run implies, config has no dir entry.
-    """
-    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
+    """First run implies, config has no dir entry."""
+
+    def mock_cwd() -> Path:
+        return tmp_path
+
+    monkeypatch.setattr(Path, 'cwd', mock_cwd)
+    # monkeypatch.setattr('Path.cwd', lambda: str(tmp_path))
+
     config_file = tmp_path / 'defaults.ini'
-    general_dir = str(tmp_path)
+    general_dir = Path(tmp_path)
 
     setup_dirs(config, config_file)
 
     # Check value in config object
-    assert config['general']['dir'] == general_dir
+    assert config['general']['dir'] == str(general_dir)
 
     # Check value in written config file
     config = configparser.ConfigParser()
-    with open(config_file) as config_file:
+    with config_file.open('r') as config_file:
         config.read_file(config_file)
-    assert config['general']['dir'] == general_dir
+    assert config['general']['dir'] == str(general_dir)
 
     # Check created directories
     isdir_helper(tmp_path)
 
 
 def test_setup_dirs_second_run(monkeypatch, tmp_path, config):
-    """Simulates a second (or third, fourth, ...) run, ie. dir in config is set and necessary directories exists.
-    """
-    monkeypatch.setattr('os.getcwd', lambda: str(tmp_path))
+    """Simulates a second (or third, fourth, ...) run, ie. dir in config is set and necessary directories exists."""
+
+    def mock_cwd() -> Path:
+        return tmp_path
+
+    monkeypatch.setattr(Path, 'cwd', mock_cwd)
+
     config_file = tmp_path / 'defaults.ini'
-    general_dir = str(tmp_path)
+    general_dir = tmp_path
 
     # First run => set everythin up
     setup_dirs(config, config_file)
