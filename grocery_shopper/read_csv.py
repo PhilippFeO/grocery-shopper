e4336cc3e02f1d8f2a7e6937e27d18e0afd2b51c
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2025-01-02 12:01:24 +0100

    fix: 🐛 Split 'icu-file' on '\n' for csv parsing

--- grocery_shopper/read_csv.py
+++ grocery_shopper/read_csv.py
@@ -12,7 +12,7 @@ class CategoryURL(NamedTuple):
 def read_csv(file_path: Path) -> dict[str, CategoryURL]:
     """Read a CSV file and returns a dictionary. First entry (name of ingredient) is key, value is tuple consisting of `category` and `url`."""
     csv_dict = {}
-    csv_reader = csv.reader(file_path.read_text())
+    csv_reader = csv.reader(file_path.read_text().split('\n'))
 
     for row in csv_reader:
         ingredient_str, category, *urls = row
